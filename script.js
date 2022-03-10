a8de3f6394ac07214fd9ea6201e6019f2fc66aac
Sean Poston <sean.poston@wwinc.com>
2022-03-10 15:22:36 -0600

    disabled rsvp

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function rsvpBoxSizing () {
-    /*
-    Purpose: if active file is the rsvp page, style the form's buttons and boxes to a certain size.
-    Author: Sean Poston
-    */
-    var currentLocation = window.location;
-    if (currentLocation['pathname'] === '/rsvp.html') {
-        document.getElementById('fname').style.height="75px";
-        document.getElementById('fname').style.fontSize="18pt";
-
-        document.getElementById('lname').style.height="75px";
-        document.getElementById('lname').style.fontSize="18pt";
-
-        document.getElementById('numattend').style.height="75px";
-        document.getElementById('numattend').style.fontSize="18pt";
-        document.getElementById('numattend').style.width="292px";
-
-        document.getElementById('submit').style.height="75px";
-        document.getElementById('submit').style.width="250px";
-        document.getElementById('submit').style.fontSize="18pt";
-    }
-}
-
 function toggleMenu () {
     /*
     Purpose: insert active class to move showcase over and display hidden menu
@@ -119,6 +96,5 @@ function calculateCountdown (weddingDate) {
     return times;
 }
 
-rsvpBoxSizing();
 toggleMenu();
 displayCountdown();
